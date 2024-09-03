5b10cbecf93c94891f111a3a69045725038e65c3
Newlife0810 <55177757+Newlife0810@users.noreply.github.com>
2024-09-03 10:29:07 +0800

    Delete packages directory

--- packages/headers/index.d.ts
+++
@@ -1,3 +0,0 @@
-export type PluginArgs = HeadersInit;
-
-export default function (args: PluginArgs): PagesFunction;
--- packages/stytch/src/api/index.ts
+++
@@ -1,4 +0,0 @@
-export const envs = {
-  live: "https://api.stytch.com/v1/",
-  test: "https://test.stytch.com/v1/",
-};
