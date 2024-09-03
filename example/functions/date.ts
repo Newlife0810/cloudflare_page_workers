538a17a77137c44e5d3cc332707e482750881803
Newlife0810 <55177757+Newlife0810@users.noreply.github.com>
2024-09-03 10:28:47 +0800

    Delete example directory

--- example/functions/date.ts
+++
@@ -1,2 +0,0 @@
-export const onRequest: PagesFunction = () =>
-  new Response(new Date().toISOString());
--- example/functions/image.tsx
+++
@@ -1,5 +0,0 @@
-import { ImageResponse } from "@cloudflare/pages-plugin-vercel-og/api";
-
-export const onRequestGet = async () => {
-  return new ImageResponse(<div>Hello, world!</div>);
-};
