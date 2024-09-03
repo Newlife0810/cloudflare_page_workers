b0f0e3b580dce6dda0f8ac05aa9835118633b623
Newlife0810 <55177757+Newlife0810@users.noreply.github.com>
2024-09-03 10:20:23 +0800

    Delete pages-plugins-main directory

--- pages-plugins-main/example/functions/throw.ts
+++
@@ -1,3 +0,0 @@
-export const onRequest: PagesFunction = () => {
-  throw new Error("Waaa");
-};
--- pages-plugins-main/packages/graphql/index.d.ts
+++
@@ -1,5 +0,0 @@
-import type { graphql, GraphQLSchema } from "graphql";
-
-export type PluginArgs = { schema: GraphQLSchema; graphql: typeof graphql };
-
-export default function (args: PluginArgs): PagesFunction;
